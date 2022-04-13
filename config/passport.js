0a4068233e6d204ce86117da3e6e54286f4142c3
OlloClark <ollo42@hotmail.co.uk>
2022-04-12 17:27:26 -0700

    adding oauth Google Strategy callback function

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-//Require your User Model here!
+const User = require("../models/user")
 
 // configuring Passport!
 passport.use(new GoogleStrategy({
@@ -8,12 +8,24 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  function(accessToken, refreshToken, profile, cb) {
-    // a user has logged in via OAuth!
-    // refer to the lesson plan from earlier today in order to set this up
-
+   function(accessToken, refreshToken, profile, cb) {
+    User.findOne({ googleId: profile.id }).then(async function(user) {
+      if (user) return cb(null, user);
+      try {
+        user = await User.create({
+          name: profile.displayName,
+          googleId: profile.id,
+          email: profile.emails[0].value,
+          avatar: profile.photos[0].value
+        });
+        return cb(null, user);
+      } catch (err) {
+        return cb(err);
+      }
+    });
   }
-));
+)
+);
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
